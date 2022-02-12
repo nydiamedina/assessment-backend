50a9bbfb75a149f4f33b51a47eff9549450bdd1c
Nydia Medina <nvmedina29@gmail.com>
2022-02-11 22:59:42 -0600

    Submitting first feature

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 
 const app = express();
 
-
 app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
@@ -18,8 +17,20 @@ app.get("/api/compliment", (req, res) => {
   let randomIndex = Math.floor(Math.random() * compliments.length);
   let randomCompliment = compliments[randomIndex];
 
-  res.status(200).send(randomCompliment);
-  
+  res.status(200).send(randomCompliment); 
+});
+
+app.get("/api/fortune", (req, res) => {
+  const fortuneCookieFortunes = ["Congratulations! You are on your way.",
+					 "Everyday in your life is a special occasion.",
+					 "Living with a commitment to excellence shall take you far.",
+  ];
+
+  // choose random fortune
+  let randomIndex = Math.floor(Math.random() * fortuneCookieFortunes.length);
+  let randomFortune = fortuneCookieFortunes[randomIndex];
+
+  res.status(200).send(randomFortune);  
 });
 
 app.listen(4000, () => console.log("Server running on 4000"));
