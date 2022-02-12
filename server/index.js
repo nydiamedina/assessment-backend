fdc7920e9f2b0bd37b30012fc702c5deb68c958e
Nydia Medina <nvmedina29@gmail.com>
2022-02-12 02:11:16 -0600

    Submitting second, third, and four features

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const userGoals = require('./db.json')
 
 const app = express();
 
@@ -31,6 +32,39 @@ app.get("/api/fortune", (req, res) => {
   let randomFortune = fortuneCookieFortunes[randomIndex];
 
   res.status(200).send(randomFortune);  
+});
+
+app.post("/api/goals", (req, res) => {
+  let userGoal = req.body;
+  userGoals.push(userGoal);
+
+  userGoal.message = `Awesome, ${userGoal.firstName}! You're one step closer to accomplish them.`
+
+  res.status(201).send(userGoal);  
+});
+
+app.delete("/api/goals/:name", (req, res) => {
+  let userFirstName  = req.params.name;
+  for (let i = 0; i < userGoals.length; i++) {
+    if (userGoals[i].firstName === userFirstName) {
+      userGoals.splice(i, 1);
+      res.status(200).send("User goal deleted.");
+      return;
+    }
+  }
+  res.status(400).send("User goal not found.")
+});
+
+app.put("/api/goals/:name/weekly", (req, res) => {
+  let userFirstName  = req.params.name;
+  for (let i = 0; i < userGoals.length; i++) {
+    if (userGoals[i].firstName === userFirstName) {
+      userGoals[i].weeklyGoal = req.body.weeklyGoal;
+      res.status(200).send(userGoals[i]);
+      return;
+    }
+  }
+  res.status(400).send("User goal not found.")
 });
 
 app.listen(4000, () => console.log("Server running on 4000"));
